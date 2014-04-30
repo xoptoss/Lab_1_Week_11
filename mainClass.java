a1dfb90cba0d93a11042c51f7a5115ea82bc012c
James Perez <xoptoss@gmail.com>
2014-04-29 19:32:34 -0700

    Create mainClass.java

---
+++ mainClass.java
@@ -0,0 +1,16 @@
+package mainPackage;
+
+public class mianClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		dice Dice = new dice();
+		
+		Dice.Throw();
+		Dice.GetValue();
+		System.out.println(Dice.value);
+		
+	}
+
+}
