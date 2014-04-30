a41e0323ef4c38b503e7692cfe7e80f3c1a9c939
James Perez <xoptoss@gmail.com>
2014-04-29 19:33:23 -0700

    Create dice.java

---
+++ dice.java
@@ -0,0 +1,21 @@
+package mainPackage;
+import java.util.Random;
+public class dice {
+int value,newValue;
+
+	Random random = new Random();
+
+	public int Throw()
+	{
+		newValue = random.nextInt()+1;
+		return newValue;
+	}
+	
+	public int GetValue()
+	{
+		value = newValue;
+		return value;
+	}
+	
+	
+}
